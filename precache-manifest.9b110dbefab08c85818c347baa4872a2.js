4d929bcc3bcc142c658e7f7ce558fa56778657a6
Theethawat Savastham <5910110150@psu.ac.th>
2019-09-08 23:51:25 +0700

    Updates

---
+++ precache-manifest.9b110dbefab08c85818c347baa4872a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144df6cc4726bb095a4d851e782bcb3f",
+    "url": "/memoriesofhatyai/index.html"
+  },
+  {
+    "revision": "9d65df79ceec7c845da2",
+    "url": "/memoriesofhatyai/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "08f3baa50da25dacbec8",
+    "url": "/memoriesofhatyai/static/css/main.fd158404.chunk.css"
+  },
+  {
+    "revision": "9d65df79ceec7c845da2",
+    "url": "/memoriesofhatyai/static/js/2.a101fddc.chunk.js"
+  },
+  {
+    "revision": "08f3baa50da25dacbec8",
+    "url": "/memoriesofhatyai/static/js/main.43e3dafb.chunk.js"
+  },
+  {
+    "revision": "2d9b22dd40340e992e71",
+    "url": "/memoriesofhatyai/static/js/runtime~main.3629cd63.js"
+  }
+]);
